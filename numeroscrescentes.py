6320eef139b3f30800c2d07c262afaa0a77521ef
amanda <amandamfnobre@gmail.com>
2024-10-12 18:34:59 -0300

    Update numeroscrescentes.py
    faster

--- numeroscrescentes.py
+++ numeroscrescentes.py
@@ -1,15 +1,20 @@
 n1 = int(input('numero 1:'))
 n2 = int(input('numero 2:'))
 n3 = int(input('numero 3:'))
-if n1 <= n2 <= n3:
-    print (n1,n2,n3)
-elif n2 <= n3 <= n1:
-    print (n2,n3,n1)
-elif n2 <= n1 <= n3:
-    print (n2,n1,n3)
-elif n3 <= n1 <= n2:
-    print (n3, n1, n2)
-elif n3 <= n2 <= n1:
-    print (n3,n2,n1)
-elif n1 <= n3 <= n2:
-    print (n1,n3,n2)
+# if n1 <= n2 <= n3:
+#     print (n1,n2,n3)
+# elif n2 <= n3 <= n1:
+#     print (n2,n3,n1)
+# elif n2 <= n1 <= n3:
+#     print (n2,n1,n3)
+# elif n3 <= n1 <= n2:
+#     print (n3, n1, n2)
+# elif n3 <= n2 <= n1:
+#     print (n3,n2,n1)
+# elif n1 <= n3 <= n2:
+#     print (n1,n3,n2)
+# else:
+#     print ('digite um numero!')
+num = [n1,n2,n3]
+num.sort()
+print num
